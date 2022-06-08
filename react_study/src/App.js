ed4d16d23efb059c751b1a6cd2b333f8eff15a38
sy071500 <sy071500@naver.com>
2022-06-08 15:17:20 +0900

    React Study

---
+++ react_study/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  const [message, setMessage] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(phoneNumber);
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    if (event.target.value.startsWith(0)) {
+      //alert("Phone Number is vaild");
+      setMessage("Phone Number is vaild");
+      setPhoneNumber(event.target.value);
+    } else if (event.target.value.length === 0) {
+      setPhoneNumber("");
+      setMessage("");
+    } else {
+      setPhoneNumber("");
+      setMessage("Phone Number should starts with 0");
+      //alert("Phone Number should starts with 0");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="phone">Phone Number: </label>
+      <br />
+      <input
+        id="phone"
+        name="phone"
+        onChange={handleChange}
+        value={phoneNumber}
+      />
+      <br />
+      <p>{message}</p>
+      <br />
+      <button
+        type="submit"
+        disabled={
+          !phoneNumber.length === 0 || message !== "Phone Number is vaild"
+        }
+      >
+        Submit
+      </button>
+      <p>{phoneNumber}</p>
+    </form>
+  );
+}
+
+export default App;
