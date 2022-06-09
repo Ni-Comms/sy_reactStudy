e61500b207831ded36238ca223a0183018f29099
sy071500 <sy071500@naver.com>
2022-06-09 10:08:24 +0900

    React Study

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -6,23 +6,23 @@ function App() {
   const [pw, setPw] = React.useState("");
 
   const handleChange = (event) => {
+    //event 사용했을 때 이것은 꼭 해주어야함
     event.preventDefault();
     if(event.target.id === "idValue"){
       if (event.target.value.length === 0) {
-      setId("");
-    } else {
-      setId(event.target.value);
-    }
+        setId("");
+      } else {
+        setId(event.target.value);
+      }
     }else if(event.target.id === "pwValue"){
       if (event.target.value.length === 0) {
-      setPw("");
+        setPw("");
       } else {
-      setPw(event.target.value);
+        setPw(event.target.value);
       }
     }
-    
   };
-
+  //컴포넌트
   const ID = () => {
       return <div>
           ID:<input id="idValue" defaultValue={id}  onChange={handleChange}/>
@@ -34,12 +34,11 @@ function App() {
           </div>
   }
   const Login = () => {
-    
       return <div>
           <button disabled={id.length === 0 || pw.length === 0}>Login</button>
           </div>
   }
-
+  //컴포넌트로 만들어서 return안에 넣어줌
   return (
     <>
       <ID />
@@ -48,5 +47,5 @@ function App() {
     </>
   );
 }
-
+//다른 곳에서 import해주기 위해서는 꼭 해줘야함
 export default App;
