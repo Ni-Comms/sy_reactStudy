bb90cf47790bbff3da15f119025c81f7f01d710a
sy071500 <sy071500@naver.com>
2022-06-13 09:11:44 +0900

    React Study:: JSX, props Study

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,42 +1,13 @@
 import React from 'react'
+import Extraction from './components/Extraction/Extraction'
+import Composition from './components/2-4.Props/Composition'
 
 function App() {
 
-  const [data, setData] = React.useState(null);
-  const [error, setError] =  React.useState(null);
-  
-  React.useEffect(() => {
-    fetch('https://raw.githubusercontent.com/techoi/raw-data-api/main/simple-api.json')
-    .then(function (response){
-      return response.json();
-    })
-    .then(function (myJson){
-      console.log(JSON.stringify(myJson));
-      setData(myJson.data);
-    })
-    .catch((error)=>{
-      //alert(error);
-      setError(error.message);
-    });
-  },[]);
-
-  if(error != null){
-    return <p>{error}</p>;
-  }
-
-  if(data==null) { 
-    return <p>Loading...</p>
-  };
-
   return (
-    <div>
-      <p>people</p>
-      {data.people.map((person)=>(
-        <>
-          <span>name: {person.name}</span>
-          <span>age: {person.age}</span>
-        </>
-      ))}
+    <div className="App">
+      <Composition/>
+      <Extraction/>
     </div>
   );
 }
