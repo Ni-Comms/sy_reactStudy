56b73284103abc3071b93cdc68f268e9b98d693c
sy071500 <sy071500@naver.com>
2022-06-13 10:08:08 +0900

    React Study::  State, 컴포넌트 생명주기 Study

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Extraction from './components/Extraction/Extraction'
 import Composition from './components/2-4.Props/Composition'
+import ClassComponent from './components/2-5.State/ClassComponent';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
 
   return (
     <div className="App">
+       <ClassComponent2/>
+      <FunctionalComponent/>
+      <ClassComponent/>
       <Composition/>
       <Extraction/>
     </div>
