853010a327abfc1e6dc2d1d8ea4afbd26c70be57
sy071500 <sy071500@naver.com>
2022-06-14 09:03:53 +0900

    React Study::  List Study

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import List from './components/2-9.List/List';
 //import Extraction from './components/Extraction/Extraction'
 //import Composition from './components/2-4.Props/Composition'
 //import ClassComponent from './components/2-5.State/ClassComponent';
 //import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 //import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 //import Event from './components/2-7.Event/Event';
-import Condition from './components/2-8.ConditionalRendering/Condition';
+//import Condition from './components/2-8.ConditionalRendering/Condition';
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
       <Composition/>
       <Extraction/> */}
       {/* <Event/> */}
-      <Condition/>
+      {/* <Condition/> */}
+      <List/>
     </div>
   );
 }
